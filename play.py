b10a10517d80ad50903435382edc22488a149765
opit <opit@hardcore.lt>
2017-05-04 14:12:02 +0300

    Perlin noise with 16 boxes

--- play.py
+++ play.py
@@ -9,6 +9,12 @@ from marimba_config import *
 octaves = [1, 2, 3, 4]
 note_values = ["C", "D", "F", "G"]
 
+def make_sequence(noteSeq, row):
+	for el in row:
+		print el, 'el'
+		noteSeq.append(Note(el[0], el[1], el[2], el[3]))
+	return noteSeq	
+
 
 def make_threshold(noise_array):
 	lower_third = np.percentile(noise_array, 33)
@@ -27,57 +33,39 @@ def get_Perlin_noise():
 	noise_array = make_threshold(60*noise_array)#midpoint between 0 and 127
 	return noise_array
 	
-duration = 10 #30 seconds, 60 quarter notes, 120 1/8th notes, 240 1/16th notes
-midi = Midi(number_tracks=16, tempo=120)
+duration = 120 #In 1/16ths. 30 seconds, 60 quarter notes, 120 1/8th notes, 240 1/16th notes
+midi = Midi(number_tracks=1, tempo=120)
 
 #fig = plt.figure()
 
-seq1 = NoteSeq([])
-seq2 = NoteSeq([])
-seq3 = NoteSeq([])
-seq4 = NoteSeq([])
-seq5 = NoteSeq([])
-seq6 = NoteSeq([])
-seq7 = NoteSeq([])
-seq8 = NoteSeq([])
 
+sequence = np.zeros((16, duration), dtype=[('value', 'i2'), ('octave', 'i2'), ('dur', 'f4'), ('vol', 'i4')])
 
-sequence = np.zeros((16, duration))
-
-for i in range(0, duration):
-	tmp = OpenSimplex(seed=i)
+for frame in range(0, duration):
+	tmp = OpenSimplex(seed=frame)
 	loudness = get_Perlin_noise()
-	for i, note in enumerate(["C", "D", "F", "G"]):
-		for j, octave in enumerate([3, 4, 5, 6]):
-			#print note, octave, loudness[i, j]
-			print loudness[i, j], 'loudness'
-			seq1.append(Note(0, 3, 0.125, loudness[i, j]))
-			seq2.append(Note(2, 4, 0.125, loudness[i, j]))
-			seq3.append(Note(5, 5, 0.125, loudness[i, j]))
-			seq4.append(Note(7, 6, 0.125, loudness[i, j]))
-			seq5.append(Note(0, 4, 0.125, loudness[i, j]))
-			seq6.append(Note(2, 3, 0.125, loudness[i, j]))
-			seq7.append(Note(5, 6, 0.125, loudness[i, j]))
-			seq8.append(Note(7, 5, 0.125, loudness[i, j]))
-
-
+	cnt = 0
+	while cnt < 16:
+		for i, note in enumerate([0, 2, 5, 7]):
+			for j, octave in enumerate([3, 4, 5, 6]):
+				print i, j, cnt, frame, "i, j, i*j, frame"
+				print 'note, octave, loudness', note, octave, loudness[i, j]
+				sequence[cnt, frame] = (note, octave, 0.125, loudness[i, j])
+				cnt += 1
+print sequence, sequence.shape
 	#c = plt.imshow(noise, interpolation=None, cmap='viridis')
 	#plt.colorbar(c)
 #plt.show()	
 	
-	
-#seq1 = NoteSeq([Note(x) for x in range(0, 12, 1)])#NoteSeq("C4 D8 R E")
-#seq2 = NoteSeq("F G A")
-#seq1 = NoteSeq([Note(0, 5, 0.25, 127), Rest(0.5), Note(5, 3, 0.5, 127)])
+
 midi = Midi(number_tracks=16, tempo=120)
-midi.seq_notes(seq1, time=0)
-midi.seq_notes(seq2, time=1)
-midi.seq_notes(seq3, time=2)
-midi.seq_notes(seq4, time=4)
-midi.seq_notes(seq5, time=5)
-midi.seq_notes(seq6, time=6)
-midi.seq_notes(seq7, time=7)
-midi.seq_notes(seq8, time=8)
+
+for row in sequence:
+		noteSeq = []
+		noteSeq = make_sequence(noteSeq, row)
+		midi.seq_notes(noteSeq, time=0)
+		
+
 midi.write("midi_output/test.mid")
 
 '''
