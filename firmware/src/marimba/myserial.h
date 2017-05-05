f86b3330065e53cf82bb6e35fbd635f616125d74
miceuz <mic@hardcore.lt>
2017-05-05 15:10:24 +0300

    Increase serial baud rate to 115200

--- firmware/src/marimba/myserial.h
+++ firmware/src/marimba/myserial.h
@@ -6,11 +6,12 @@ class MySerial{
 
   public:
   	void begin(uint16_t baudrate) {
-	    #define BAUD 9600
+	    #define BAUD 115200
 	    #include <util/setbaud.h>
 	    UBRR0H = UBRRH_VALUE;
 	    UBRR0L = UBRRL_VALUE;
-      UCSR0B = _BV(RXCIE0) | _BV(RXEN0);
+    	UCSR0A |= _BV(U2X0);
+    	UCSR0B = _BV(RXCIE0) | _BV(RXEN0);
 	    UCSR0C = _BV(UCSZ00) | _BV(UCSZ01);
   	}
 
