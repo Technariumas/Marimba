dbc78879a38aaf2ece34cf9e36529cc330dcd598
opit <opit@hardcore.lt>
2017-05-09 21:38:12 +0300

    rolling through rows

---
+++ firmware/util/row_chase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+from marimba import Marimba
+import time
+import numpy as np
+
+m = Marimba()
+m.connect()
+
+print("Rolling rows...")
+index_array = np.reshape(np.arange(80), (8, 10)).T
+while True:
+	for row in index_array:
+		m.playSequence(row, 0)
+		time.sleep(0.25)
+	time.sleep(2)
+
