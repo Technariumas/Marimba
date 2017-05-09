55b4d4a0dc899a30ac9c4a720e841020f927a687
opit <opit@hardcore.lt>
2017-05-09 21:08:30 +0300

    column chase, test

--- firmware/util/column_chase.py
+++ firmware/util/column_chase.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python
 from marimba import Marimba
 import time
+import numpy as np
 
 m = Marimba()
 m.connect()
 
 print("Rolling columns...")
 index_array = np.reshape(np.arange(80), (8, 10)).T
-print(index_array.shape)
 while True:
-	m.play(2)
+	m.play([2, 3, 4])
 	time.sleep(0.5)
 	m.stop(2)
 	time.sleep(2)
