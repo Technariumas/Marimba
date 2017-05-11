9e6d33016114c4d1fd473c49e6a9d0f5bd117fc8
opit <opit@hardcore.lt>
2017-05-11 15:22:54 +0300

    play single note boxes

--- data_analysis/test_midi.py
+++ data_analysis/test_midi.py
@@ -19,13 +19,16 @@ noteSeq = []
 #for t in range(0, 80):
 #	noteSeq.append(Note(t, 0, 0.125, 127))
 
-for i, region in enumerate([1, 2, 3, 4]): #iterating over 4 notes
-			for j, octave in enumerate(octaves): #iterating over 4*4 frames (setting octave values)
-				for frame in range(60): 
-					current_box = get_boxes(notes[i], octave)
-					playing = index_array[current_box]
-					print playing, "current_box", current_box
-				midi.seq_notes(noteSeq, time=0)
+for i, region in enumerate([1]): #iterating over 4 notes
+		noteSeq = []
+		for frame in range(60): 
+			print notes[0]
+			current_box = get_boxes(notes[0], 3)
+			playing = index_array[current_box]
+			for i,play in np.ndenumerate(playing):
+				noteSeq.append(Note(play, 0, 0.125, 127))
+			print playing, "current_box", current_box
+		midi.seq_notes(noteSeq, time=0)
 midi.write("midi_output/test_pyknon.mid")
 
 
