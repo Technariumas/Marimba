00f3d3585995db32362ecc5c350204ea19de3093
opit <opit@hardcore.lt>
2017-05-15 13:14:07 +0300

    testing, rendering test sequence

--- data_analysis/test_midi.py
+++ data_analysis/test_midi.py
@@ -21,10 +21,14 @@ noteSeq = []
 
 for i, region in enumerate([1, 2, 3, 4]): #iterating over 4 notes
 			for j, octave in enumerate(octaves): #iterating over 4*4 frames (setting octave values)
+				noteSeq = []
 				for frame in range(60): 
 					current_box = get_boxes(notes[i], octave)
 					playing = index_array[current_box]
 					print "time:", frame, playing, "current_box", current_box
+					for box in playing:
+						noteSeq.append(Note(box, 0, 0.25, 60))
+				print noteSeq
 				midi.seq_notes(noteSeq, time=0)
 midi.write("midi_output/test_pyknon.mid")
 
