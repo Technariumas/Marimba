1d341f9a6123ceed93f794665522b8d06f2cb3b9
opit <opit@hardcore.lt>
2017-05-19 19:01:21 +0300

    address boxes

--- firmware/util/highStrokeCalibrator.py
+++ firmware/util/highStrokeCalibrator.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 import pickle
 from marimba import Marimba
+import sys
+
 m=Marimba()
 m.connect()
+
+box_number = sys.argv[1]
 
 try:
 	with open('highStroke', 'rb') as fp:
@@ -10,22 +14,45 @@ try:
 except EOFError:
 	highStroke = [17] * 80
 
-for n in range(80):
-	print("Dabartine jega: " + str(highStroke[n]))
-	m.setHighStrokeLength(n, highStroke[n])
-	while True:
-		key = raw_input("Patiko? (T/N)")
-		if 'T' == key or 't' == key:
-			with open('highStroke', 'wb') as fp:
-				pickle.dump(highStroke, fp)
-			break
-		else:
-			val = raw_input("Ivesk smugio jega [0 - 127] gera pradzia - 17:")
-			try:
-				i = int(val)
-				if i >= 0 and i <= 127:
-					highStroke[n] = i
-					m.setHighStrokeLength(n, i)
-			except ValueError:
-				print("Vesk tik skaicius!")
-				m.test(n)
+
+
+if box_number = "all":
+	for n in range(80):
+		print("Dabartine jega: " + str(highStroke[n]))
+		m.setHighStrokeLength(n, highStroke[n])
+		while True:
+			key = raw_input("Patiko? (T/N)")
+			if 'T' == key or 't' == key:
+				with open('highStroke', 'wb') as fp:
+					pickle.dump(highStroke, fp)
+				break
+			else:
+				val = raw_input("Ivesk smugio jega [0 - 127] gera pradzia - 17:")
+				try:
+					i = int(val)
+					if i >= 0 and i <= 127:
+						highStroke[n] = i
+						m.setHighStrokeLength(n, i)
+				except ValueError:
+					print("Vesk tik skaicius!")
+					m.test(n)
+	else:
+		n = int(box_number)
+		print("Dabartine jega: " + str(highStroke[n]))
+		m.setHighStrokeLength(n, highStroke[n])
+		while True:
+			key = raw_input("Patiko? (T/N)")
+			if 'T' == key or 't' == key:
+				with open('highStroke', 'wb') as fp:
+					pickle.dump(highStroke, fp)
+				break
+			else:
+				val = raw_input("Ivesk smugio jega [0 - 127] gera pradzia - 17:")
+				try:
+					i = int(val)
+					if i >= 0 and i <= 127:
+						highStroke[n] = i
+						m.setHighStrokeLength(n, i)
+				except ValueError:
+					print("Vesk tik skaicius!")
+					m.test(n)
