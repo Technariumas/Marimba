7bbec636106e775359a5b57bdebff1dda920124a
miceuz <mic@hardcore.lt>
2017-05-19 14:44:25 +0300

    beat testing

--- firmware/util/test_beat.py
+++ firmware/util/test_beat.py
@@ -7,13 +7,15 @@ m.connect()
 
 #volume = int(sys.argv[1])
 box = int(sys.argv[1])
-duration = int(sys.argv[2])
-length = int(sys.argv[3])
+volume = int(sys.argv[3])
+duration = float(sys.argv[4])
+length = int(sys.argv[2])
+
 
 print "playing box #", box
 while True:
-	sequence = [box for box in range(0, length)]
+	sequence = [box for i in range(0, length)]
 	print sequence
 	m.playSequence(sequence, 0, duration)
-	sleep(10)
+	time.sleep(10)
 #notes, delay, duration
