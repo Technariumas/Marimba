4c95e18889c39a50a18f662ea67e288c81b4382b
opit <opit@hardcore.lt>
2017-05-19 14:47:14 +0300

    test beat

--- firmware/util/test_beat.py
+++ firmware/util/test_beat.py
@@ -5,17 +5,15 @@ import time
 m = Marimba()
 m.connect()
 
-#volume = int(sys.argv[1])
 box = int(sys.argv[1])
+length = int(sys.argv[2])
 volume = int(sys.argv[3])
 duration = float(sys.argv[4])
-length = int(sys.argv[2])
 
-
+print "Usage: python test_beat.py [box number] [number of notes] [volume] [duration]"
 print "playing box #", box
-while True:
-	sequence = [box for i in range(0, length)]
-	print sequence
-	m.playSequence(sequence, 0, duration)
-	time.sleep(10)
+sequence = [box for i in range(0, length)]
+print sequence
+m.playSequence(sequence, 0, duration)
+time.sleep(10)
 #notes, delay, duration
