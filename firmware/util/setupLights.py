11da09b73dc1c98b6014ba0a557721ebb7478ca2
miceuz <mic@hardcore.lt>
2017-05-22 00:59:02 +0300

    all parameter reset

--- firmware/util/setupLights.py
+++ firmware/util/setupLights.py
@@ -11,6 +11,9 @@ m.connect()
 sleep(1)
 [m.setLightCount(n, 4) for n in range(80)]
 sleep(1)
+[m.setLightMinimum(n, 15) for n in range(80)]
+sleep(1)
+
 [m.setLightMaximum(n, 127) for n in range(80)]
 sleep(1)
 [m.setLightMaximum(n, 60) for n in [randrange(60, 80) for i in range(3)]]
