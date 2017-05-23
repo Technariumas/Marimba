e7ff36a7fe002eb2bc1ad5d068ce1014af1e71ab
opit <opit@hardcore.lt>
2017-05-23 13:03:50 +0300

    x

--- data_analysis/utils.py
+++ data_analysis/utils.py
@@ -10,6 +10,7 @@ db.generate_mapping(create_tables=True)
 
 
 def make_threshold(array, note):
+	print note, "note", array.shape
 	if note == 0:
 		lowest = np.percentile(array, 50)
 		lower = np.percentile(array, 67)
@@ -49,46 +50,48 @@ def window(size):
     
 @db_session
 def get_list_of_districts():
-	return db.select("select distinct raj from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end")
+	return db.select("select distinct raj from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end")
 
 @db_session
 def get_districts():
-	return db.select("select raj from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end")
+	return db.select("select raj from data_0516 where time_start>= $time_slice_start and time_start < $time_slice_end")
 
 @db_session
 def get_mb():
-	return db.select("select mb from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end")
+	return db.select("select mb from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end")
 
 @db_session
 def get_time_start():
-	return db.select("select time_start from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end order by time_start")
+	return db.select("select time_start from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end order by time_start")
 
 @db_session
 def get_session_count(region):
-	return db.select("select count(time_start) from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end and cell_grp = $region group by time_start")
-
+	return db.select("select count(time_start) from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end and cell_grp = $region")
 
 @db_session
 def get_unique_times_start(region):
-	return db.select("select distinct time_start from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end and cell_grp = $region order BY time_start")
+	return db.select("select distinct time_start from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end and cell_grp = $region")
 
 @db_session
 def get_time_end():
-	return db.select("select time_finish from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end")
-
+	return db.select("select time_finish from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end order by time_start")
+ 
 @db_session
 def get_mb_in_region(region):
-	return db.select("select mb from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end and cell_grp = $region order BY time_start")
+	return db.select("select mb from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end and cell_grp = $region order BY time_start")
 
-@db_session
+#@db_session
+#def get_duration():
+#	return (datetime.strptime(time_slice_end, '%Y-%m-%d %H:%M:%S') - datetime.strptime(time_slice_start, '%Y-%m-%d %H:%M:%S')).seconds
+
 def get_time_start_in_region(region):
-	return db.select("select time_start from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end and cell_grp = $region")
+	return db.select("select time_start from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end and cell_grp = $region order by time_start")
 
 @db_session
 def get_session_duration():
-	return db.select("select datediff(time_finish,time_start) from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end order BY time_start")
+	return db.select("select datediff(time_finish,time_start) from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end")
 
 @db_session
 def mb_per_second_in_region(region):
-	return db.select("sum(mb) as sum_mb from data_0516 where time_start > $time_slice_start and time_start < $time_slice_end and cell_grp = $region GROUP BY time_start")
+	return db.select("sum(mb) as sum_mb from data_0516 where time_start >= $time_slice_start and time_start < $time_slice_end and cell_grp = $region GROUP BY time_start")
 
