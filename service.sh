6c40e4c99a9b980950004e8a5360c7dcbfd98737
miceuz <mic@hardcore.lt>
2017-05-28 20:04:10 +0300

    service

--- service.sh
+++ service.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-ttymidi -b 115200 -s /dev/serial0 #&
+ttymidi -b 115200 -s /dev/serial0 &
 #sleep 1
 #/home/pi/Marimba/firmware/util/blastem.py
+cd /home/pi/Marimba
+git pull
+aplaymidi -p 128:1 /home/pi/Marimba/data_analysis/midi_output/play.mid &
+cd /home/pi/Marimba/firmware/util
+python lights_control.py 
 
-#aplaymidi -p 128:1 arba play.sh
+
