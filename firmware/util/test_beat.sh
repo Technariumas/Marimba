c0ef7a8c48cf117ec1da20f3d12ce893fe7226e6
opit <opit@hardcore.lt>
2017-05-30 20:17:59 +0300

    test beat baudrate

--- firmware/util/test_beat.sh
+++ firmware/util/test_beat.sh
@@ -1,4 +1,4 @@
-ttymidi -b 115200 -s /dev/serial0 &
+ttymidi -b 9600 -s /dev/serial0 &
 echo "box number, number of notes, volume, note duration" $1, $2, $3, $4
 ./test_beat.py $1 $2 $3 $4
 killall ttymidi
